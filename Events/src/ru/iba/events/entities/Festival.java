c864c6de203c4754a1e3a8f890767c2fbea676e7
chipallino1 <skorupich00@mail.ru>
2018-10-03 18:19:13 +0300

    class for description festival

--- Events/src/ru/iba/events/entities/Festival.java
+++ Events/src/ru/iba/events/entities/Festival.java
@@ -2,65 +2,17 @@ package ru.iba.events.entities;
 
 public class Festival extends Event {
 
-	protected int enterancePrice;
-	
-	public int getEnterancePrice()
-	{
-		return enterancePrice;
-	}
-	public boolean setEnterancePrice(int enterancePriceSetting)
-	{
-		if(enterancePriceSetting>=0)
-			return true;
-		return false;
-	}
 	@Override
-	public int getID() {
-		// TODO Auto-generated method stub
-		return ID;
-	}
-
-	@Override
-	public String getName() {
-		// TODO Auto-generated method stub
-		return Name;
-	}
-
-	@Override
-	public String getDate() {
-		// TODO Auto-generated method stub
-		return Date;
-	}
-
-	@Override
-	public boolean setID(int idSetting) {
-
-		if(idSetting>0)
+	public boolean addPurchasedPlaces(int addingPurchasedPlaces) {
+		
+		if(addingPurchasedPlaces>0)
 		{
-			ID=idSetting;
+			purchasedPlaces=purchasedPlaces+addingPurchasedPlaces;
+			
 			return true;
 		}
 		return false;
-	}
-
-	@Override
-	public boolean setName(String nameSetting) {
-		if(nameSetting!=null && !nameSetting.isEmpty())
-			{
-				Name=nameSetting;
-				return true;			
-			}
-		return false;
-	}
-
-	@Override
-	public boolean setDate(String dateSetting) {
-		if(dateSetting!=null && !dateSetting.isEmpty())
-		{
-			Date=dateSetting;
-			return true;
-		}
-		return false;
+		
 	}
 
 	
