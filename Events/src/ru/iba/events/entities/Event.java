6175c5f4cfdd802ce9804a7d22872d2dac94a595
chipallino1 <skorupich00@mail.ru>
2018-10-03 18:18:17 +0300

    abstract class for main characters

--- Events/src/ru/iba/events/entities/Event.java
+++ Events/src/ru/iba/events/entities/Event.java
@@ -1,17 +1,69 @@
 package ru.iba.events.entities;
 
 public abstract class Event {
-	protected int ID;
-	protected String Name;
-	protected String Date;
+	int id;
+	int enterancePrice;
+	String name;
+	String date;
+	int purchasedPlaces;			//  общее кол-во купленных мест
 	
-	abstract public int getID();
-	abstract public String getName();
-	abstract public String getDate();
+	public abstract boolean addPurchasedPlaces(int addingPurchasedPlaces);
 	
-	abstract public boolean setID(int idSetting);
-	abstract public boolean setName(String nameSetting);
-	abstract public boolean setDate(String dateSetting);
+	
+	public int getEnterancePrice()
+	{
+		return enterancePrice;
+	}
+	
+	public int getID() {
+		// TODO Auto-generated method stub
+		return id;
+	}
+	public String getName() {
+		// TODO Auto-generated method stub
+		return name;
+	}
+
+	public String getDate() {
+		// TODO Auto-generated method stub
+		return date;
+	}
+	
+	public boolean setEnterancePrice(int enterancePriceSetting)
+	{
+		if(enterancePriceSetting>=0)
+			return true;
+		return false;
+	}
+
+	public boolean setID(int idSetting) {
+
+		if(idSetting>0)
+		{
+			id=idSetting;
+			return true;
+		}
+		return false;
+	}
+
+	public boolean setName(String nameSetting) {
+		if(nameSetting!=null && !nameSetting.isEmpty())
+			{
+				name=nameSetting;
+				return true;			
+			}
+		return false;
+	}
+
+	public boolean setDate(String dateSetting) {
+		if(dateSetting!=null && !dateSetting.isEmpty())
+		{
+			date=dateSetting;
+			return true;
+		}
+		return false;
+	}
+
 	
 	
 }
