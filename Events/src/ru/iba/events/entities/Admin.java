99c411fc37cabc19ee34c403d3c8318795d3ade6
chipallino1 <skorupich00@mail.ru>
2018-10-03 18:16:37 +0300

    class for admin user

---
+++ Events/src/ru/iba/events/entities/Admin.java
@@ -0,0 +1,12 @@
+package ru.iba.events.entities;
+
+//стадия разработки
+public class Admin extends User {
+
+	@Override
+	public Performers[] getPerformers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
