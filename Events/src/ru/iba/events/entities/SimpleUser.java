8cf6ca3a37afa4af4ceecde0100a7664afd18c2c
chipallino1 <skorupich00@mail.ru>
2018-10-03 18:21:29 +0300

    class for description usual user

---
+++ Events/src/ru/iba/events/entities/SimpleUser.java
@@ -0,0 +1,12 @@
+package ru.iba.events.entities;
+
+//стадия разработки
+public class SimpleUser extends User {
+
+	@Override
+	public Performers[] getPerformers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
