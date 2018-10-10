a30b5700840709d44c2634ff2c882d080e5ea3e8
chipallino1 <skorupich00@mail.ru>
2018-10-10 16:07:17 +0300

    add setter getter to MembersPerformers.java

--- Events/src/ru/iba/events/entities/MembersPerformer.java
+++ Events/src/ru/iba/events/entities/MembersPerformer.java
@@ -3,9 +3,74 @@ package ru.iba.events.entities;
 public class MembersPerformer {
 	
 	private String name;
-	private int age;
+	private String age;
+	private String position;
 	private String additional;
 	
+	public String getName()
+	{
+		return name;
+	}
+	public String getAge()
+	{
+		return age;
+	}
+	public String getPosition()
+	{
+		return position;
+	}
+	public String getAdditional()
+	{
+		return additional;
+	}
 	
+	public boolean setName(String Name) {
+
+		if(Name==null)
+		{
+			return false;//will add a "throw new exc"
+		}
+		if(Name=="")
+		{
+			return false;//will add a "throw new exc"
+		}
+		name=Name;
+		return true;
+		
+	}
+	public boolean setAge(String Age)
+	{
+		
+		if(Age==null)
+		{
+			return false;//will add a "throw new exc"
+		}
+		if(Age=="")
+		{
+			return false;//will add a "throw new exc"
+		}
+		age=Age;
+		return true;
+		
+	}
+	public boolean setPosition(String Position)
+	{
+
+		if(Position==null)
+		{
+			return false;//will add a "throw new exc"
+		}
+		if(Position=="")
+		{
+			return false;//will add a "throw new exc"
+		}
+		position=Position;
+		return true;
+	}
+	public boolean setAdditional(String Additional)
+	{
+		additional=Additional;
+		return true;
+	}
 	
 }
