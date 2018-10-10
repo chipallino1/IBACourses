1d73eb32034a56cd6dc0e6b6ea0752fe5e10d832
chipallino1 <skorupich00@mail.ru>
2018-10-10 16:17:39 +0300

    add setter getter to Performers.java

--- Events/src/ru/iba/events/entities/Performers.java
+++ Events/src/ru/iba/events/entities/Performers.java
@@ -7,5 +7,51 @@ public class Performers {
 	private int id;
 	private String name;	
 	private MembersPerformer[] members;//участники фирмы на фестивале/участники группы на концерте
+	
+	public int getId()
+	{
+		return id;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public MembersPerformer[] getMembers()
+	{
+		return members;
+	}
+	
+	public boolean setId(int id)
+	{
+		if(id<0)
+			return false;
+		return true;
+	}
+	public boolean setName(String Name)
+	{
+		if(Name==null)
+		{
+			return false;//will add a "throw new exc"
+		}
+		if(Name=="")
+		{
+			return false;//will add a "throw new exc"
+		}
+		name=Name;
+		return true;
+	}
+	public boolean setMembers(MembersPerformer[] Members)
+	{
+		if(Members==null)
+		{
+			return false;//will add a "throw new exc"
+		}
+		if(Members.length==0)
+		{
+			return false;//will add a "throw new exc"
+		}
+		members=Members;
+		return true;
+	}
 
 }
